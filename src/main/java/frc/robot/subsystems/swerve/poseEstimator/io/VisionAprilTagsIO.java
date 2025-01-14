8743c35be36a1d3ecbac86accf8bafd036397c0e
Yonatan Vigiser <yonatanvigiser@gmail.com>
2025-01-14 21:47:53 +0200

    change viosionIO field name to plural

--- src/main/java/frc/robot/subsystems/swerve/poseEstimator/io/VisionAprilTagsIO.java
+++ src/main/java/frc/robot/subsystems/swerve/poseEstimator/io/VisionAprilTagsIO.java
@@ -7,8 +7,8 @@ import frc.lib.logfields.IOBase;
 import frc.lib.logfields.LogFieldsTable;
 
 public abstract class VisionAprilTagsIO extends IOBase {
-    public final Supplier<Pose3d[]> poseEstimates = fields.addObjectArray("poseEstimate", this::getRobotPoses, new Pose3d[0]);
-    public final Supplier<double[]> cameraTimestampsSeconds = fields.addDoubleArray("cameraTimestampSeconds",
+    public final Supplier<Pose3d[]> posesEstimates = fields.addObjectArray("poseEstimates", this::getRobotPoses, new Pose3d[0]);
+    public final Supplier<double[]> cameraTimestampsSeconds = fields.addDoubleArray("cameraTimestampsSeconds",
             this::getCameraTimestampsSeconds);
     public final Supplier<Pose3d[][]> tagsPoses = fields.addObjectMatrix("tagsPoses", this::getTagsPoses, new Pose3d[0][0]);
     public final Supplier<double[][]> tagsAmbiguitys = fields.addDoubleMatrix("tagsAmbiguitys", this::getTagsAmbiguitys);
