6725b829a0772e9fe9496f2bcc8e9a487fea00fb
Yonatan Vigiser <yonatanvigiser@gmail.com>
2025-01-28 17:13:12 +0200

    Added Funnel networkAlerts

--- src/main/java/frc/robot/subsystems/funnel/Funnel.java
+++ src/main/java/frc/robot/subsystems/funnel/Funnel.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems.funnel;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.logfields.LogFieldsTable;
 import frc.robot.Robot;
@@ -17,8 +19,15 @@ public class Funnel extends SubsystemBase {
     private final LogFieldsTable fieldsTable = new LogFieldsTable(getName());
     private final Debouncer isCoralInDebouncer = new Debouncer(DEBOUNCER_SECONDS, DebounceType.kBoth);
 
+    private final Alert isCoralInAlert = new Alert("Funnel", "Coral Not Inside", AlertType.kWarning);
+
     public Funnel() {
         io = Robot.isReal() ? new FunnelIOSparksMax(this.fieldsTable) : new FunnelIOSim(this.fieldsTable);
+    }
+
+    @Override
+    public void periodic() {
+        isCoralInAlert.set(io.isCoralIn.getAsBoolean());
     }
 
     public void setMotorVoltage(double voltageDemand) {
