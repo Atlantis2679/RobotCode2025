5af6c952d01f40570bda5bf4ae411e96385b15f5
Yonatan Vigiser <yonatanvigiser@gmail.com>
2025-01-29 11:53:32 +0200

    Big changes to NetworkAlerts
    Added the option to create a subgroup, added some static methods, fixed robot status not synchronizing by removing it from the created list and updating manualy, renamed and reformat.

--- src/main/java/frc/robot/subsystems/NetworkAlerts.java
+++ src/main/java/frc/robot/subsystems/NetworkAlerts.java
@@ -11,139 +11,191 @@ import edu.wpi.first.wpilibj.Alert.AlertType;
 
 public class NetworkAlerts {
     private final static List<NetworkAlerts> createdNetworksAlerts = new ArrayList<>();
-    private final static String parentGroupName = "networkAlerts/";
+    private final static String parentFolder = "networkAlerts";
 
-    private final static NetworkAlerts generalNetworkAlerts = new NetworkAlerts("General Status", false);
+    private final static NetworkAlerts robotStatusNetworkAlerts = new NetworkAlerts(
+        parentFolder, "Robot Status", false);
 
     private final Map<Alert, BooleanSupplier> alerts = new HashMap<>();
 
     private final String groupName;
+    private final String groupFolder;
 
-    private NetworkAlerts(String groupName, boolean addToGeneral) {
-        this.groupName = parentGroupName + groupName;
-        if (addToGeneral) {
-            generalNetworkAlerts.addInfoAlert(groupName + " Is OK", this::isOK);
-            generalNetworkAlerts.addWarningAlert(groupName + " Has Warnings!", this::hasWarnings);
-            generalNetworkAlerts.addErrorAlert(groupName + " Has Errors!", this::hasErrors);
+    private boolean hasWarnings = false;
+    private boolean hasErrors = false;
+
+    private NetworkAlerts(String parentGroupFolder, String groupName, boolean addToRobotStatus) {
+        this.groupName = groupName;
+        this.groupFolder = parentGroupFolder + "/" + groupName;
+        if (addToRobotStatus) {
+            robotStatusNetworkAlerts.addInfoAlert(groupName + " Is OK", this::getIsOK);
+            robotStatusNetworkAlerts.addWarningAlert(groupName + " Has Warnings!", this::getHasWarnings);
+            robotStatusNetworkAlerts.addErrorAlert(groupName + " Has Errors!", this::getHasErrors);
+            createdNetworksAlerts.add(this);
         }
-        createdNetworksAlerts.add(this);
     }
     
     public NetworkAlerts(String groupName) {
-        this(groupName, true);
+        this(parentFolder, groupName, true);
     }
 
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public String getGroupFolder() {
+        return groupFolder;
+    }
+
+    public boolean getHasErrors() {
+        return hasErrors;
+    }
+
+    public boolean getHasWarnings() {
+        return hasWarnings && !hasErrors;
+    }
+
+    public boolean getIsOK() {
+        return !(hasErrors || hasWarnings);
+    }
+
+    /* A sub group is recorded seperatly from it's parent in the robot status alerts */
+    public NetworkAlerts getSubGroup(String subGroupName) {
+        return new NetworkAlerts(groupFolder, subGroupName, true);
+    }
+    
     public void addAlert(String message, AlertType alertType, BooleanSupplier isActive) {
-        alerts.put(new Alert(groupName, message, alertType), isActive);
+        alerts.put(new Alert(groupFolder + "/General", message, alertType), isActive);
     }
 
-    public void addAlert(String name , String message, AlertType alertType, BooleanSupplier isActive) {
-        alerts.put(new Alert(groupName + "/" + name, message, alertType), isActive);
+    /* A subGroupName is a way to create a group that isn't ment to be shown seperate from his
+     * parent group. For example, each module may be in a group named "Modules",
+     * but it can contain more than one alert (each motor has it's own status alert) */
+ 
+    public void addAlert(String subGroupName, String message, AlertType alertType, BooleanSupplier isActive) {
+        alerts.put(new Alert(groupFolder + "/" + subGroupName, message, alertType), isActive);
     }
 
     public void addInfoAlert(String message, BooleanSupplier isActive) {
         addAlert(message, AlertType.kInfo, isActive);
     }
 
-    public void addInfoAlert(String name , String message, BooleanSupplier isActive) {
-        addAlert(name, message, AlertType.kInfo, isActive);
+    public void addInfoAlert(String subGroupName, String message, BooleanSupplier isActive) {
+        addAlert(subGroupName, message, AlertType.kInfo, isActive);
     }
 
     public void addWarningAlert(String message, BooleanSupplier isActive) {
         addAlert(message, AlertType.kWarning, isActive);
     }
-
-    public void addWarnignAlert(String name , String message, BooleanSupplier isActive) {
-        addAlert(name, message, AlertType.kWarning, isActive);
+    
+    public void addWarningAlert(String subGroupName, String message, BooleanSupplier isActive) {
+        addAlert(subGroupName, message, AlertType.kWarning, isActive);
     }
 
     public void addErrorAlert(String message, BooleanSupplier isActive) {
         addAlert(message, AlertType.kError, isActive);
     }
-
-    public void addErrorAlert(String name , String message, BooleanSupplier isActive) {
-        addAlert(name, message, AlertType.kError, isActive);
+    
+    public void addErrorAlert(String subGroupName, String message, BooleanSupplier isActive) {
+        addAlert(subGroupName, message, AlertType.kError, isActive);
     }
 
-    public void addSwitchAlert(String activeMessage, String unactiveMessage, AlertType unactiveAlertType, AlertType activeAlertType,
-            BooleanSupplier isActive) {
+    public void addSwitchAlert(String activeMessage, String unactiveMessage, AlertType activeAlertType, 
+                               AlertType unactiveAlertType, BooleanSupplier isActive) {
         addAlert(activeMessage, activeAlertType, isActive);
         addAlert(unactiveMessage, unactiveAlertType, () -> !isActive.getAsBoolean());
     }
-
-    public void addSwitchAlert(String name, String activeMessage, String unactiveMessage, AlertType unactiveAlertType,
-            AlertType activeAlertType, BooleanSupplier isActive) {
-        addAlert(activeMessage, activeAlertType, isActive);
-        addAlert(unactiveMessage, unactiveAlertType, () -> !isActive.getAsBoolean());
+    
+    public void addSwitchAlert(String subGroupName, String activeMessage, String unactiveMessage, 
+                               AlertType activeAlertType, AlertType unactiveAlertType, BooleanSupplier isActive) {
+        addAlert(subGroupName, activeMessage, activeAlertType, isActive);
+        addAlert(subGroupName, unactiveMessage, unactiveAlertType, () -> !isActive.getAsBoolean());
     }
 
-    public void addInfoInfoSwitchAlert(String firstInfoMessage, String secondInfoMessage, BooleanSupplier isFirstActive) {
+    public void addInfoInfoSwitchAlert(String firstInfoMessage, String secondInfoMessage, 
+                                       BooleanSupplier isFirstActive) {
         addSwitchAlert(firstInfoMessage, secondInfoMessage, AlertType.kInfo, AlertType.kInfo, isFirstActive);
     }
 
-    public void addInfoInfoSwitchAlert(String name, String firstInfoMessage, String secondInfoMessage, BooleanSupplier isFirstActive) {
-        addSwitchAlert(name, firstInfoMessage, secondInfoMessage, AlertType.kInfo, AlertType.kInfo, isFirstActive);
+    public void addInfoInfoSwitchAlert(String subGroupName, String firstInfoMessage, String secondInfoMessage, 
+                                       BooleanSupplier isFirstActive) {
+        addSwitchAlert(subGroupName, firstInfoMessage, secondInfoMessage, AlertType.kInfo,
+                       AlertType.kInfo, isFirstActive);
     }
 
-    public void addInfoWarningSwitchAlert(String infoMessage, String warningMessage, BooleanSupplier isInfoActive) {
+    public void addInfoWarningSwitchAlert(String infoMessage, String warningMessage, 
+                                          BooleanSupplier isInfoActive) {
         addSwitchAlert(infoMessage, warningMessage, AlertType.kInfo, AlertType.kWarning, isInfoActive);
     }
 
-    public void addInfoWarningSwitchAlert(String name, String infoMessage, String warningMessage, BooleanSupplier isInfoActive) {
-        addSwitchAlert(name, infoMessage, warningMessage, AlertType.kInfo, AlertType.kWarning, isInfoActive);
+    public void addInfoWarningSwitchAlert(String subGroupName, String infoMessage, String warningMessage, 
+                                          BooleanSupplier isInfoActive) {
+        addSwitchAlert(subGroupName, infoMessage, warningMessage, AlertType.kInfo, AlertType.kWarning, 
+                       isInfoActive);
     }
 
-    public void addInfoErrorSwitchAlert(String infoMessage, String errorMessage, BooleanSupplier isInfoActive) {
+    public void addInfoErrorSwitchAlert(String infoMessage, String errorMessage,
+                                        BooleanSupplier isInfoActive) {
         addSwitchAlert(infoMessage, errorMessage, AlertType.kInfo, AlertType.kError, isInfoActive);
     }
 
-    public void addInfoErrorSwitchAlert(String name, String infoMessage, String errorMessage, BooleanSupplier isInfoActive) {
-        addSwitchAlert(name, infoMessage, errorMessage, AlertType.kInfo, AlertType.kError, isInfoActive);
+    public void addInfoErrorSwitchAlert(String subGroupName, String infoMessage, String errorMessage, 
+                                        BooleanSupplier isInfoActive) {
+        addSwitchAlert(subGroupName, infoMessage, errorMessage, AlertType.kInfo, AlertType.kError, 
+                       isInfoActive);
     }
 
-    public void addWarningErrorSwitchAlert(String warningMessage, String errorMessage, BooleanSupplier isWarningActive) {
-        addSwitchAlert(warningMessage, errorMessage, AlertType.kWarning, AlertType.kError, isWarningActive);
+    public void addWarningErrorSwitchAlert(String warningMessage, String errorMessage, 
+                                           BooleanSupplier isWarningActive) {
+        addSwitchAlert(warningMessage, errorMessage, AlertType.kWarnng, AlertType.kError, isWarningActive);
     }
 
-    public void addWarningErrorSwitchAlert(String name, String warningMessage, String errorMessage, BooleanSupplier isWarningActive) {
-        addSwitchAlert(name, warningMessage, errorMessage, AlertType.kWarning, AlertType.kError, isWarningActive);
+    public void addWarningErrorSwitchAlert(String subGroupName, String warningMessage, String errorMessage, 
+                                           BooleanSupplier isWarningActive) {
+        addSwitchAlert(subGroupName, warningMessage, errorMessage, AlertType.kWarnng, 
+                       AlertType.kError, isWarningActive);
     }
 
-    public void addStatusAlert(String message, BooleanSupplier isInfoActive, BooleanSupplier isWarningActive, BooleanSupplier isErrorActive) {
+    public void addStatusAlert(String message, BooleanSupplier isInfoActive, 
+                               BooleanSupplier isWarningActive, BooleanSupplier isErrorActive) {
         addInfoAlert(message, isInfoActive);
         addWarningAlert(message, isWarningActive);
         addErrorAlert(message, isErrorActive);
     }
 
-    public void addStatusAlert(String name, String message, BooleanSupplier isInfoActive, BooleanSupplier isWarningActive, BooleanSupplier isErrorActive) {
-        addInfoAlert(name, message, isInfoActive);
-        addWarnignAlert(name,message, isWarningActive);
-        addErrorAlert(name,message, isErrorActive);
+    public void addStatusAlert(String subGroupName, String message, BooleanSupplier isInfoActive, 
+                               BooleanSupplier isWarningActive, BooleanSupplier isErrorActive) {
+        addInfoAlert(subGroupName, message, isInfoActive);
+        addWarningAlert(subGroupName, message, isWarningActive);
+        addErrorAlert(subGroupName, message, isErrorActive);
     }
 
     public static void update() {
         for (NetworkAlerts networkAlerts : createdNetworksAlerts) {
             networkAlerts.alerts.forEach((alert, isActive) -> {
                 alert.set(isActive.getAsBoolean());
+                if(alert.get() && alert.getType() == AlertType.kWarning)
+                    networkAlerts.hasWarnings = true;
+                if(alert.get() && alert.getType() == AlertType.kError)
+                    networkAlerts.hasErrors = true;
             });
         }
+        /* Done seperatly in order that the robot status will be synchronized with */
+        /* the hasWarnings and hasErrors.                                          */
+        robotStatusNetworkAlerts.forEach((alert, isActive) -> {
+                alert.set(isActive.getAsBoolean());
+        });
     }
 
-    public boolean hasErrors() {
-        for (Alert alert : alerts.keySet()) {
-            if(alert.get() && alert.getType() == AlertType.kError)
-                return true;
-        } return false;
-    }
+    /* static methods to make the alerts data more accesible */
 
-    public boolean hasWarnings() {
-        for (Alert alert : alerts.keySet()) {
-            if(alert.get() && alert.getType() == AlertType.kWarning)
-                return true;
-        } return false;
-    }
-
-    public boolean isOK() {
-        return !(hasErrors() || hasWarnings());
+    public static NetworkAlerts getGroupByName(String groupName) {
+        for(NetworkAlerts networkAlerts : createdNetworksAlerts) {
+            if(networkAlerts.getGroupName().equals(groupName))
+                return networkAlerts;
+        }
+        if(robotStatusNetworkAlerts.getGroupName().eqals(groupName)
+           return robotStatusNetworkAlerts;
+        return null;
     }
 }
