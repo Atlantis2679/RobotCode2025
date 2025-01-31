485d9963cceeb468ff3562fbbe324bea21997fd4
Yonatan Vigiser <yonatanvigiser@gmail.com>
2025-01-31 13:36:00 +0200

    Updated the RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -24,9 +24,8 @@ public class RobotMap {
         public static final int FUNNEL_LEFT_MOTOR_ID = 0;
         public static final int FUNNEL_RIGHT_MOTOR_ID = 0;
 
-        public static final int GRIPPER_LEFT_OUTTAKE_MOTOR_ID = 0;
-        public static final int GRIPPER_RIGHT_OUTTAKE_MOTOR_ID = 0;
-        public static final int GRIPPER_IBACK_MOTOR_ID = 0;
+        public static final int GRIPPER_LEFT_MOTOR_ID = 0;
+        public static final int GRIPPER_RIGHT_MOTOR_ID = 0;
 
         public class ModuleFL {
             public final static int DRIVE_MOTOR_ID = 20;
