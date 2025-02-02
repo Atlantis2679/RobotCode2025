04edf118c263d354d86b046648c97a3b42b7b50a
Yonatan Vigiser <yonatanvigiser@gmail.com>
2025-02-02 21:44:38 +0200

    started AllCommands

---
+++ src/main/java/frc/robot/allcommands/AllCommandsConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.allcommands;
+
+public class AllCommandsConstants {
+    public static final double L3_Angle = 85; //need to check that
+    public static final double L1_Angle = 0;
+    public static final double Delivery_Angle = 0;//we need to select an angle
+    public static final double PIVOT_ANGLE_FOR_INTAKE = 0;
+    
+
+}
