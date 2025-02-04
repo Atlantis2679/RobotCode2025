0faff7e837b881de2e3c57f078ae8609f7d27ca0
Yonatan Vigiser <yonatanvigiser@gmail.com>
2025-02-04 16:55:35 +0200

    Part of last commit

--- src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
@@ -5,7 +5,4 @@ public class FunnelConstants {
 
     public static final double MOTORS_MAX_VOLTAGE = 12;
     public static final double DEBOUNCER_SECONDS = 0.05;
-
-    public static final double MOTORS_PERCENTAGE_SPEED_LOADING = 0.35;
-    public static final double MOTORS_PERCENTAGE_SPEED_PASSING = 0.35;
 }
