68e5ef2ad3a51c231936590255cf95a777b44e48
Yonatan Vigiser <yonatanvigiser@gmail.com>
2025-03-04 21:54:35 +0200

    Not yet finished - start working on leds

--- src/main/java/frc/robot/subsystems/leds/LedsCommands.java
+++ src/main/java/frc/robot/subsystems/leds/LedsCommands.java
@@ -1,47 +1,17 @@
 package frc.robot.subsystems.leds;
 
-import java.util.function.Consumer;
-
-import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public class LedsCommands {
+    private final Leds leds;
 
-    public static Command colorForSeconds(Color color, double seconds, Leds... ledStrips) {
-        return new SequentialCommandGroup(
-            new InstantCommand(() -> runForLEDs(ledStrip -> ledStrip.staticColor(color), ledStrips)),
-            new WaitCommand(seconds),
-            new InstantCommand(() -> {
-                runForLEDs(ledStrip -> ledStrip.clearLedColors(), ledStrips);
-            })
-        );
-    }
-    public static Command getStaticColorCommand(Color color, Leds... ledStrips) {
-        return new StartEndCommand(
-                () -> runForLEDs((ledStrip -> ledStrip.staticColor(color)), ledStrips),
-                () -> {
-                },
-                ledStrips
-        ).ignoringDisable(true);
-    }
-    public static Command clearLeds(Leds...ledStrips){
-        return new InstantCommand(() -> runForLEDs((ledStrip -> ledStrip.clearLedColors()), ledStrips));
-    }
-    public static Command getBlinkingCommand(Color color, double blinkingIntervalSeconds, Leds... ledStrips) {
-        return new RunCommand(
-                () -> runForLEDs((ledStrip -> ledStrip.blink(color, blinkingIntervalSeconds)), ledStrips),
-                ledStrips
-        ).ignoringDisable(true);
+    public LedsCommands(Leds leds) {
+        this.leds = leds;
     }
 
-    public static void runForLEDs(Consumer<Leds> action, Leds... ledStrips) {
-        for (Leds ledStrip : ledStrips)
-            action.accept(ledStrip);
-    }
-
+    // public Command rainbow() {
+    //     // return Commands.runOnce(leds.applyPatern(LEDPattern.rainbow(0, 0));, null)
+    // }
 }
--- src/main/java/frc/robot/subsystems/leds/LedsConstants.java
+++ src/main/java/frc/robot/subsystems/leds/LedsConstants.java
@@ -1,22 +1,6 @@
 package frc.robot.subsystems.leds;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import frc.robot.RobotMap;
+public final class LedsConstants {
+    public static final int LEDS_LENGHT = 74;
 
-public class LedsConstants {
-    public final static int TOTAL_LEDS_NUMBER = 74;
-
-    static final AddressableLEDBuffer LED_BUFFER = new AddressableLEDBuffer(TOTAL_LEDS_NUMBER);
-    static final AddressableLED LED = new AddressableLED(RobotMap.LEDS_ID);
-    
-    public static final Leds
-            RIGHT_CLIMBER_LEDS = new Leds(TOTAL_LEDS_NUMBER, 0);
-            // LEFT_CLIMBER_LEDS = new Leds(LEDS_LEFT_LENGTH, LEDS_RIGHT_LENGTH - 1);
-    
-    static {
-        LED.setLength(TOTAL_LEDS_NUMBER);
-        LED.setData(LED_BUFFER);
-        LED.start();
-    }
 }
