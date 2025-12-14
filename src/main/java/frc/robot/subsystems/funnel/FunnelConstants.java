d066810fcee1775c81350a3e55fb20cb309978d6
YonatanBrot <yonatanbrot@gmail.com>
2025-12-14 17:37:54 +0200

    created the SparkMaxIo

--- src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.funnel;
 
 public class FunnelConstants {
-    public static double DEBOUNCER_DELAY = 0.8;
-    public static double PRECENT_TO_VOLTAGE = 12;
+    public final static double DEBOUNCER_DELAY = 0.8;
+    public final static double PRECENT_TO_VOLTAGE = 12;
+
+    //Added by me
+    public final static int MAX_CURRENT = 10;
 }
