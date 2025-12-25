286e46a876feabce1374289c25b0284c7e7f41ca
Ariel Izbinsky <ariel.izbinsky@gmail.com>
2025-12-25 18:00:40 +0200

    updated the elevator based on the pivot, still have to rethink the logic and make sure I actually know how to write it

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -27,31 +27,27 @@ public class Elevator extends SubsystemBase {
 
   private final RotationalSensorHelper elevatorRotationalSensorHelper;
 
-  private final TunableTrapezoidProfile pivotTrapezoid = new TunableTrapezoidProfile(
+  private final TunableTrapezoidProfile elevatorTrapezoid = new TunableTrapezoidProfile(
             new TrapezoidProfile.Constraints(MAX_VELOCITY_DEG_PER_SEC, MAX_ACCELERATION_DEG_PER_SEC_SQUARED));
 
-  private PIDController pivotPidController = new PIDController(KP, KI, KD);
+  private PIDController elevatorPidController = new PIDController(KP, KI, KD);
 
-  private TunableArmFeedforward pivotFeedforward = Robot.isSimulation()
-            ? new TunableArmFeedforward(Sim.SIM_KS, Sim.SIM_KG, Sim.SIM_KV, Sim.SIM_KA)
-            : new TunableArmFeedforward(KS, KG, KV);
+  private TunableArmFeedforward elevatorFeedforward = Robot.isReal()
+            ? new TunableArmFeedforward(KS, KG, KV)
+            : new TunableArmFeedforward(Sim.SIM_KS, Sim.SIM_KG, Sim.SIM_KV, Sim.SIM_KA);
 
     private double maxAngle = MAX_ANGLE_DEGREES;
     private double minAngle = MIN_ANGLE_DEGREES;
-
-    private double upperBound = UPPER_BOUND;
-    private double lowerBound = LOWER_BOUND;
 
     private final Debouncer encoderConnectedDebouncer = new Debouncer(ENCODER_CONNECTED_DEBAUNCER_SEC);
 
   public Elevator(){
     fieldsTable.update();
     elevatorRotationalSensorHelper = new RotationalSensorHelper(io.encoderAngle.getAsDouble(), ANGLE_OFFSET);
-    elevatorRotationalSensorHelper.enableContinuousWrap(lowerBound, upperBound);
 
     TunablesManager.add("Elevator", (Tunable) this);
 
-    PeriodicAlertsGroup.defaultInstance.addErrorAlert(() -> "Pivot: Encoder Disconnected!", () -> !getEncoderConnectedDebouncer());
+    PeriodicAlertsGroup.defaultInstance.addErrorAlert(() -> "Elevator: Encoder Disconnected!", () -> !getEncoderConnectedDebouncer());
   }
 
   @Override
@@ -59,9 +55,6 @@ public class Elevator extends SubsystemBase {
     fieldsTable.recordOutput("Encoder angle", io.getEncoderAngle());
     fieldsTable.recordOutput("Right motor current", io.getRightMotorCurrent());
     fieldsTable.recordOutput("Left motor current", io.getLeftMotorCurrent());
-  }
-  public boolean getEncoderConnectedDebouncer() {
-    return encoderConnectedDebouncer.calculate(io.isEncoderConnected.getAsBoolean());
   }
   public void setVoltage(double voltage) {
     if((getAngle() > MAX_ANGLE_DEGREES && voltage > 0)
@@ -72,16 +65,35 @@ public class Elevator extends SubsystemBase {
     fieldsTable.recordOutput("voltage", voltage);
     io.setVoltage(voltage);
   }
+  public void stop(){
+    io.setVoltage(0);
+  }
   public double getAngle(){
     return io.getEncoderAngle();
+  }
+  public double getVelocity() {
+    return elevatorRotationalSensorHelper.getVelocity();
+  }
+  public boolean getEncoderConnectedDebouncer() {
+    return encoderConnectedDebouncer.calculate(io.isEncoderConnected.getAsBoolean());
+  }
+  public double calculateFeedForward(double desiredAngleDegrees, double desiredSpeed, boolean usePID) {
+    fieldsTable.recordOutput("desired angle", desiredAngleDegrees);
+    fieldsTable.recordOutput("desired speed", desiredSpeed);
+    double speed = elevatorFeedforward.calculate(Math.toRadians(desiredAngleDegrees), desiredSpeed);
+    if (usePID && !isAtAngle(desiredAngleDegrees)) {
+        speed += elevatorPidController.calculate(getAngle(), desiredAngleDegrees);
+    }
+    return speed;
+  }
+  public TrapezoidProfile.State calculateTrapezoidProfile(double time, TrapezoidProfile.State initialState,
+            TrapezoidProfile.State goalState) {
+        return elevatorTrapezoid.calculate(time, initialState, goalState);
   }
   public boolean isAtAngle(double desiredAngleDegrees) {
     return Math.abs(desiredAngleDegrees - getAngle()) <= ANGLE_TOLERENCE_DEGREES;
   }
   public void resetPID() {
-    pivotPidController.reset();
-  }
-  public void stop(){
-    io.setVoltage(0);
+    elevatorPidController.reset();
   }
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorCommands.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorCommands.java
@@ -2,18 +2,63 @@ package frc.robot.subsystems.elevator;
 
 import static frc.robot.subsystems.elevator.ElevatorConstants.*;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.Command;
+import team2679.atlantiskit.valueholders.ValueHolder;
 
 public class ElevatorCommands {
   private Elevator elevator;
 
-  public ElevatorCommands(Elevator elevator){
+  public ElevatorCommands(Elevator elevator) {
     this.elevator = elevator;
   }
-  public void setHeight(double desiredheight){
-    elevator.setVoltage();
+
+  public Command moveToAngle(DoubleSupplier desiredAngleDeg) {
+    ValueHolder<TrapezoidProfile.State> referenceState = new ValueHolder<TrapezoidProfile.State>(null);
+    return elevator.runOnce(() -> {
+      elevator.resetPID();
+      referenceState.set(new TrapezoidProfile.State(elevator.getAngle(), elevator.getVelocity()));
+    }).andThen(elevator.run(() -> {
+      // if (referenceState.get().position == desiredAngleDeg.getAsDouble()
+      // && Math.abs(elevator.getAngleDegrees() - desiredAngleDeg.getAsDouble()) > 40)
+      // referenceState.set(new TrapezoidProfile.State(elevator.getAngleDegrees(),
+      // elevator.getVelocity()));
+
+      referenceState.set(elevator.calculateTrapezoidProfile(
+          0.02,
+          referenceState.get(),
+          new TrapezoidProfile.State(desiredAngleDeg.getAsDouble(), 0)));
+
+      double voltage = elevator.calculateFeedForward(
+          referenceState.get().position,
+          referenceState.get().velocity,
+          true);
+
+      elevator.setVoltage(voltage);
+    })).withName("elevatorMoveToAngle");
   }
-  public double getHeight(){
-    return MOTOR_HEIGHT + CARRIAGE_LENGTH * Math.cos(elevator.getAngle() * 2 * Math.PI);
+
+  public Command setHeight(double desiredHeight) {
+    double ratio = (desiredHeight - MOTOR_HEIGHT) / CARRIAGE_LENGTH;
+
+    // Safety clamp (acos only works for [-1, 1])
+    ratio = Math.max(-1.0, Math.min(1.0, ratio));
+
+    double angle = Math.acos(ratio) / (2 * Math.PI);
+
+    return moveToAngle(() -> angle);
+  }
+
+  public Command manualController(DoubleSupplier elevatorSpeed) {
+    return elevator.run(() -> {
+      double demandSpeed = elevatorSpeed.getAsDouble();
+
+      double feedForward = elevator.calculateFeedForward(elevator.getAngle(), 0, false);
+
+      elevator.setVoltage(feedForward + demandSpeed * MAX_VOLTAGE);
+    }).withName("elevatorManualController");
   }
 }
